37a0a782bfd87db75e5aa596b5bb913f24d6efdc
Junaid Bharwana <102123268+Junaid-Bharwana@users.noreply.github.com>
2025-12-19 12:13:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const qrCodes = new Map();
 const connectionStatus = new Map();
 
 // CONFIGURATION
-const PHP_API_URL = 'http://localhost/wa3/api.php'; // CHANGE THIS FOR LIVE HOSTING
+const PHP_API_URL = 'https://w.junaidinsights.com/api.php'; // CHANGE THIS FOR LIVE HOSTING
 const INTERNAL_SECRET = 'wa3_internal_secret_key_12345';
 
 // Logger
@@ -304,4 +304,5 @@ process.on('uncaughtException', (err, origin) => {
 
 app.listen(PORT, () => {
     console.log(`WhatsApp Sender (Baileys) running on port ${PORT}`);
+
 });
